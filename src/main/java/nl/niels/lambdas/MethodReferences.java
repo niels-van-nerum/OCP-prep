4bd9fdd79390218dafc94ef653731b2eeb8e1f2d
Niels Van Nerum <vannerumniels@icloud.com>
2026-06-27 22:03:00 +0200

    WIP

--- src/main/java/nl/niels/lambdas/MethodReferences.java
+++ src/main/java/nl/niels/lambdas/MethodReferences.java
@@ -1,8 +1,25 @@
 package nl.niels.lambdas;
 
+import java.util.function.Predicate;
+
+/**
+ * Method references are shorthand versions of writing a lambda expression.
+ * </p>
+ * Sometimes you will see that when writing a basic lambda, it can be s
+ */
 public class MethodReferences {
 
     static void main() {
+        boundMethodReference();
+    }
 
+    /**
+     *
+     */
+    static void boundMethodReference() {
+        String name = "Niels";
+
+        Predicate<String> startsWith = name::startsWith;
+        System.out.println(startsWith.test("N"));
     }
 }
